f536e9c4be181948ac377202df19a2546fd9b7f8
Colton Barboro <cbarboro@gmail.com>
2020-09-14 10:07:42 -0700

    finished linear demo

--- raw_data_testing_Geology_Strips.py
+++ raw_data_testing_Geology_Strips.py
@@ -3,25 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 
-excel_file = 'D:\Data analyzation folder\Excell_data_files\Geology Meter Power Strips.xlsx'
+excel_file = 'D:\Data analyzation folder\Excell_data_files\Raw_Geology_Data.xlsx'
 
-data = pd.read_excel(excel_file, 
-    sheet_name='Post Power Strips Holiday Break', header=[20, 21])
+#Dataframe object
+data = pd.read_excel(excel_file,  header=[20, 21])
 
-dataHeaders = pd.concat(data.values(), axis=0)
+#cocatenate the header rows into one row
+data.columns = (pd.Series(data.columns.get_level_values(0)).apply(str)
+                + pd.Series(data.columns.get_level_values(1)).apply(str))
 
-# data.set_index([('* Start', '* Date'),
-#                 ('Start', 'Time'),
-#                 ('VAt', 'Avg')], inplace=True)
 
-#print(data.index.get_level_values(2))
+# kWh calculations
+data['kWh'] = data['VAtAvg'] / 1000
+data['kWh/5min'] = data['kWh'] * 5 * 60 / 3600
 
-print(dataHeaders)
+# kWh/5min monthly and cumulative sums
+watSum = data.groupby(['* Start* Date']).sum()
+watSumTotal = watSum.sum()
+watSumPrint = watSum.loc[:, ['kWh/5min']]
 
-#data['kWh'] = data[('VAt', 'Avg')] / 1000
+watData = data.loc[:, ['* Start* Date', 'StartTime', 'kWh/5min']]
 
-# watData = data.loc[:, ['kWh']]
+#print data and sums
+print(watSumPrint)
+print(watSumTotal.loc['kWh/5min'])
+print(watData)
 
-# print(data[:, ('VAt Avg')])
+# Plot data over 5 minute intervals
+watData.plot(xlabel = 'Time', ylabel = 'kWh/5min', y='kWh/5min')
+x_min, x_max = plt.xlim()
+plt.xticks(np.arange(0, x_max, 250), rotation=45)
+plt.show()
 
-#Multi indexing is probably the solution for easier reading.
+# Plot sums over monthly intervals
+watSumPrint.plot(xlabel = 'Time', ylabel = 'kWh/5min', y='kWh/5min')
+plt.show()
+
+#output data into an excel sheet
+allData = data.loc[:, ['* Start* Date', 'StartTime', 'VAtAvg', 'kWh', 'kWh/5min']]
+allData.to_excel("output.xlsx")
