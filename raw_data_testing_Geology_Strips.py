a12b1e96857cc5274562f29efe1bf4890f626511
Colton Barboro <cbarboro@gmail.com>
2020-10-26 10:10:07 -0700

    update of all work as of now

--- raw_data_testing_Geology_Strips.py
+++ raw_data_testing_Geology_Strips.py
@@ -13,32 +13,63 @@ data.columns = (pd.Series(data.columns.get_level_values(0)).apply(str)
                 + pd.Series(data.columns.get_level_values(1)).apply(str))
 
 
-# kWh calculations
-data['kWh'] = data['VAtAvg'] / 1000
-data['kWh/5min'] = data['kWh'] * 5 * 60 / 3600
+# kW calculations
+data['kW'] = data['VAtAvg'] / 1000
+data['kWh/5min'] = data['kW'] * 5 * 60 / 3600
+
+# create Time column which combines the start date and time into one column
+data['Time'] = data['* Start* Date'].apply(str) + " " + data['StartTime'].apply(str)
+data['Time'] = pd.to_datetime(data['Time'])
+print(data['Time'])
+
+# pricing calculations
+data['kW Price'] = data['kW'] * 18.1143296
+data['kWh Price'] = data['kWh/5min'] * 0.038559558
 
 # kWh/5min monthly and cumulative sums
 watSum = data.groupby(['* Start* Date']).sum()
-watSumTotal = watSum.sum()
-watSumPrint = watSum.loc[:, ['kWh/5min']]
+watSumTotal = watSum.cumsum()
+watSumPrint = watSum.loc[:, ['kW', 'kW Price', 'kWh/5min', 'kWh Price']]
+watSumTotalPrint = watSumTotal.loc[:, ['kW', 'kW Price', 'kWh/5min', 'kWh Price']]
 
+#format watData output
 watData = data.loc[:, ['* Start* Date', 'StartTime', 'kWh/5min']]
 
 #print data and sums
 print(watSumPrint)
-print(watSumTotal.loc['kWh/5min'])
+print(watSumTotalPrint)
 print(watData)
+
+
+
+
+##############################Plotting and formatting###########################
+
+
+
 
 # Plot data over 5 minute intervals
 watData.plot(xlabel = 'Time', ylabel = 'kWh/5min', y='kWh/5min')
 x_min, x_max = plt.xlim()
 plt.xticks(np.arange(0, x_max, 250), rotation=45)
-plt.show()
+plt.savefig('kWh_5min.png')
 
 # Plot sums over monthly intervals
 watSumPrint.plot(xlabel = 'Time', ylabel = 'kWh/5min', y='kWh/5min')
-plt.show()
+plt.savefig('kWh_5min_Monthly_Intervals.png')
 
-#output data into an excel sheet
-allData = data.loc[:, ['* Start* Date', 'StartTime', 'VAtAvg', 'kWh', 'kWh/5min']]
-allData.to_excel("output.xlsx")
+# Format date to print in Excel
+data['* Start* Date'] = data['* Start* Date'].dt.strftime('%m/%d/%Y')
+
+# Output data into an excel sheet
+allData = data.loc[:, ['* Start* Date', 'StartTime', 'VAtAvg', 'kW', 'kW Price', 'kWh/5min', 'kWh Price']]
+sumData = watSumPrint
+cumSumData = watSumTotalPrint
+sumData = sumData.join(cumSumData, lsuffix='_sum', rsuffix='_cumsum')
+
+with pd.ExcelWriter("data_analysis.xlsx", engine="xlsxwriter") as writer:
+    allData.to_excel(writer, sheet_name="sheet 1")
+    sumData.to_excel(writer, sheet_name="sheet 1", startcol=8)
+    worksheet = writer.sheets['sheet 1']
+    worksheet.insert_image('J17', 'kWh_5min.png')
+    worksheet.insert_image('J40', 'kWh_5min_Monthly_Intervals.png')
